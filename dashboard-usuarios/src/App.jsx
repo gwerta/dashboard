4980a8485c819c48cd30f8c5302ffda57dfa5e6e
GABRIEL DE LIMA CARNIATTO <gabriel.carniatto@etec.sp.gov.br>
2025-05-22 15:20:49 -0300

    banana

---
+++ dashboard-usuarios/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, use } from 'react';
+import UserCard from './components/UserCard';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/peoples')
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error('Erro ao buscar usuários:', err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Dashboard de Usuários</h1>
+      <p>Total de usuários: {users.length}</p>
+      <div className="user-container">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App
