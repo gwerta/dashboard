649ec7b7225a57d7ff1bcb42a4332c70fb9a4922
GABRIEL DE LIMA CARNIATTO <gabriel.carniatto@etec.sp.gov.br>
2025-05-29 15:41:41 -0300

    so falta o cartao fofo kk

--- dashboard-usuarios/src/App.jsx
+++ dashboard-usuarios/src/App.jsx
@@ -4,22 +4,49 @@ import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [paginaAtual, setPaginaAtual] = useState(1);
+  const usersPorPag = 5;
 
   useEffect(() => {
-    fetch('http://localhost:3000/people')
+    fetch('http://localhost:3000/peoples')
       .then((res) => res.json())
       .then((data) => setUsers(data))
       .catch((err) => console.error('Erro ao buscar usuários:', err));
   }, []);
+
+  const indexUltimoUser = paginaAtual * usersPorPag;
+  const indexPrimeiroUser = indexUltimoUser - usersPorPag;
+  const usersAtuais = users.slice(indexPrimeiroUser, indexUltimoUser);
+
+  const proxPag = () => {
+    if (paginaAtual < Math.ceil(users.length / usersPorPag)) {
+      setPaginaAtual(paginaAtual + 1);
+    }
+  };
+
+  const antPag = () => {
+    if (paginaAtual > 1) {
+      setPaginaAtual(paginaAtual - 1);
+    }
+  };
 
   return (
     <div className="App">
       <h1>Dashboard de Usuários</h1>
       <p>Total de usuários: {users.length}</p>
       <div className="user-container">
-        {users.map((user) => (
+        {usersAtuais.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
+      </div>
+      <div className="paginas">
+        <button onClick={antPag} disabled={paginaAtual === 1}>
+          &lt;
+        </button>
+        <span>Página {paginaAtual} de {Math.ceil(users.length / usersPorPag)}</span>
+        <button onClick={proxPag} disabled={paginaAtual === Math.ceil(users.length / usersPorPag)}>
+           &gt;
+        </button>
       </div>
     </div>
   );
